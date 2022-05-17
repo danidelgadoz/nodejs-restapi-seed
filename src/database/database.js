25f820c15f049523e82cbed1801ced91ba237064
Daniel Delgado <dedd1993@gmail.com>
2022-05-16 21:56:18 -0500

    refactor: add dotenv to handle environment variables on .env file

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,11 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('blogdb', 'postgres', 'secret', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+export const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER_NAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  }
+);
