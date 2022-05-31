61fb19af0c1e899ff8e99d864974a010784bf6e3
Daniel Delgado <dedd1993@gmail.com>
2022-05-30 19:18:37 -0500

    refactor(env): add production environment and support ssl

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,19 @@
 import { Sequelize } from 'sequelize';
 
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+};
+
 export const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER_NAME,
+  process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
     dialect: 'postgres',
+    ...(process.env.PRODUCTION === 'true' && { dialectOptions }),
   }
 );
